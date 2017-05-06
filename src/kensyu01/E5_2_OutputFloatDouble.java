86b99e87761afea40ef8be708b2a847503aa51e7
ddt-nishiwaki <ddt.jissyusei2@gmail.com>
2017-05-06 10:00:59 +0900

    演習5-2 100%

---
+++ src/kensyu01/E5_2_OutputFloatDouble.java
@@ -0,0 +1,64 @@
+package kensyu01;
+
+// 入力した値を扱う機能を読み込む
+import java.util.Scanner;
+
+/********************************************************/
+
+// 2つの値の入力を受つけ、それぞれ float,double型の値として出力するクラス
+public class E5_2_OutputFloatDouble {
+
+	// float型の値入力を求めるメッセージを設定する
+	public static final String MESSAGE_REQUEST_FLOAT = "float型の値を設定します。 : ";
+	// double型の値入力を求めるメッセージを設定する
+	public static final String MESSAGE_REQUEST_DOUBLE = "double型の値を設定します。 : ";
+	// テキスト「float型」を設定する
+	public static final String TEXT_FLOAT = "float型";
+	// テキスト「double型」を設定する
+	public static final String TEXT_DOUBLE = "double型";
+	// テキスト「出力する」を設定する
+	public static final String TEXT_OUTPUT = "出力する";
+
+
+	/********************************************************/
+
+	// 2つの値の入力を受つけ、それぞれ float,double型の値として出力する処理を行う
+	public static void main(String[] arg) {
+
+		// float型の値入力を扱うオブジェクトを作成する
+		Scanner floatInput = new Scanner(System.in);
+		// double型の値入力を扱うオブジェクトを作成する
+		Scanner doubleInput = new Scanner(System.in);
+
+
+		/********************************************************/
+
+		// float型の値入力を求めるメッセージを出力する
+		System.out.println(MESSAGE_REQUEST_FLOAT);
+		// 入力された値をfloat型で保持する
+		float floatValue = floatInput.nextFloat();
+
+		// double型の値入力を求めるメッセージを出力する
+		System.out.println(MESSAGE_REQUEST_DOUBLE);
+		// 入力された値をdouble型で保持する
+		double doubleValue = doubleInput.nextDouble(); 
+
+		// 入力されたfloat型の値を出力するメッセージを表示する
+		System.out.print(TEXT_FLOAT+"で"+TEXT_OUTPUT);
+		// 入力されたfloat型の値を出力する
+		System.out.println(floatValue);
+
+		// 入力されたdouble型の値を出力するメッセージを表示する
+		System.out.print(TEXT_DOUBLE+"で"+TEXT_OUTPUT);
+		// 入力されたdouble型の値を出力する
+		System.out.println(doubleValue);
+
+
+		/********************************************************/
+
+		// float型入力ストリームを閉じる
+		floatInput.close();
+		// double型入力ストリームを閉じる
+		doubleInput.close();
+	}
+}
