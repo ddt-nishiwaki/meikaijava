52c8238b2e944003d6c67108bbf446a860b8f6a5
ddt-nishiwaki <ddt.jissyusei2@gmail.com>
2017-05-06 10:02:56 +0900

    演習5-3 100%

---
+++ src/kensyu01/E5_3_OutputBooleanValue.java
@@ -0,0 +1,25 @@
+package kensyu01;
+
+// 設定したboolean型変数の値を文字列で出力するクラス
+public class E5_3_OutputBooleanValue {
+
+	// trueの値を設定する
+	public static final boolean VALUE_OF_TRUE = true;
+	// falseの値を設定する
+	public static final boolean VALUE_OF_FALSE = false;
+	// VALUE_OF_TRUEの値を出力する際のメッセージを設定する
+	public static final String MESSAGE_OF_TRUE = "VALUE_OF_TRUEの値は";
+	// VALUE_OF_FALSEの値を出力する際のメッセージを設定する
+	public static final String MESSAGE_OF_FALSE = "VALUE_OF_FALSEの値は";
+
+	/************************************************************/
+
+	// 設定したboolean型変数の値を文字列で出力する処理を行う
+	public static void main(String[] arg){
+
+		// VALUE_OF_TRUEの値を出力する
+		System.out.println(MESSAGE_OF_TRUE+VALUE_OF_TRUE);
+		// VALUE_OF_FALSEの値を出力する
+		System.out.println(MESSAGE_OF_FALSE+VALUE_OF_FALSE);
+	}
+}
