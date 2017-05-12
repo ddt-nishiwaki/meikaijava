d9a8278e71ac8deebb0d2166c7291d096b88ccdb
ddt-nishiwaki <ddt.jissyusei2@gmail.com>
2017-05-12 19:28:33 +0900

    演習5-4 修正100% : 定数をprivateにする, スキャナーの名前を具体的にする

--- src/kensyu01/E5_4_Avarage_Of_Input_Three.java
+++ src/kensyu01/E5_4_Avarage_Of_Input_Three.java
@@ -10,22 +10,22 @@ import java.util.Scanner;
 public class E5_4_Avarage_Of_Input_Three {
 
 	// int型変数を初期化するための値を設定する
-	public static final int INITIALIZE_INTEGER = 0;
+	private static final int INITIALIZE_INTEGER = 0;
 	// 平均値を実数で求めるための値を設定する
-	public static final double DOUBLE_DIVISOR_THREE = 3.0;
+	private static final double DOUBLE_DIVISOR_THREE = 3.0;
 
 	// 整数入力を求めるメッセージを設定する
-	public static final String MESSAGE_REQEST_INPUT = "整数値を入力してください";
+	private static final String MESSAGE_REQEST_INPUT = "整数値を入力してください";
 	// テキスト「1つめ」を設定する
-	public static final String TEXT_FIRST_TIMES = "1つめ";
+	private static final String TEXT_FIRST_TIMES = "1つめ";
 	// テキスト「2つめ」を設定する
-	public static final String TEXT_SECOND_TIMES = "2つめ";
+	private static final String TEXT_SECOND_TIMES = "2つめ";
 	// テキスト「3つめ」を設定する
-	public static final String TEXT_THIRD_TIMES = "3つめ";
+	private static final String TEXT_THIRD_TIMES = "3つめ";
 	// テキスト「合計」を設定する
-	public static final String TEXT_SUM = "合計";
+	private static final String TEXT_SUM = "合計";
 	// テキスト「平均」を設定する
-	public static final String TEXT_AVARAGE = "平均";
+	private static final String TEXT_AVARAGE = "平均";
 
 	
 	/*************************************************************************/
@@ -34,7 +34,7 @@ public class E5_4_Avarage_Of_Input_Three {
 	public static void main(String[] arg) {
 		
 		// 入力した値を扱うオブジェクトを作成する
-		Scanner integerInput = new Scanner(System.in);
+		Scanner scannerOperandInteger = new Scanner(System.in);
 		// 1つめの入力された整数を保持するための変数を初期化する
 		int firstValue = INITIALIZE_INTEGER;
 		// 2つめの入力された整数を保持するための変数を初期化する
@@ -50,17 +50,17 @@ public class E5_4_Avarage_Of_Input_Three {
 		// 1つめの整数入力を求めるメッセージを出力する
 		System.out.println(TEXT_FIRST_TIMES + "の" + MESSAGE_REQEST_INPUT);
 		// 入力された値を保持する
-		firstValue = integerInput.nextInt();
+		firstValue = scannerOperandInteger.nextInt();
 
 		// 2つめの整数入力を求めるメッセージを出力する
 		System.out.println(TEXT_SECOND_TIMES + "の" + MESSAGE_REQEST_INPUT);
 		// 入力された値を保持する
-		secondValue = integerInput.nextInt();
+		secondValue = scannerOperandInteger.nextInt();
 
 		// 3つめの整数入力を求めるメッセージを出力する
 		System.out.println(TEXT_THIRD_TIMES + "の" + MESSAGE_REQEST_INPUT);
 		// 入力された値を保持する
-		thirdValue = integerInput.nextInt();
+		thirdValue = scannerOperandInteger.nextInt();
 		
 		// 入力された値の合計値を保持する
 		sumValue = firstValue + secondValue + thirdValue;
@@ -74,6 +74,6 @@ public class E5_4_Avarage_Of_Input_Three {
 		/*************************************************************************/
 
 		// 整数値入力ストリームを閉じる
-		integerInput.close();
+		scannerOperandInteger.close();
 	}
 }
