6dfd2e6c7c2c27560893d3fd75e2cb8a28350edb
ddt-nishiwaki <ddt.jissyusei2@gmail.com>
2017-05-12 19:40:31 +0900

    演習6-7 修正100% : 定数をprivateにする, スキャナーの名前を具体的にする, 出力メッセージを追加する

--- src/kensyu01/E6_7_GetLastArrayValue.java
+++ src/kensyu01/E6_7_GetLastArrayValue.java
@@ -12,25 +12,23 @@ import java.util.Random;
 public class E6_7_GetLastArrayValue {
 
 	// 数値型変数を初期化する値を設定する
-	public static final int INITIALIZE_NUMERIC = 0;
+	private static final int INITIALIZE_NUMERIC = 0;
 	// 要素数の入力を求めるメッセージを設定する
-	public static final String REQUEST_ARRAY_LENGTH = "配列の長さを設定してください";
+	private static final String REQUEST_ARRAY_LENGTH = "配列の長さを設定してください";
 	// 配列の全要素の値をを出力する旨のメッセージを設定する
-	public static final String MESSAGE_OUTPUT_ALL = "配列の全要素の値を出力します";
+	private static final String MESSAGE_OUTPUT_ALL = "配列の全要素の値を出力します";
 	// 末尾の値を出力する旨のメッセージのフォーマットを設定する
-	public static final String FORMAT_OUTPUT_LAST = "配列の末尾の値は %d です";
-	// 文字列型変数を初期化するための値を設定する
-	public static final String INITIALIZE_STRING = "";
+	private static final String FORMAT_OUTPUT_LAST = "配列の末尾の値は %d です";
 	// 各値の区切り文字を設定する
-	public static final String VALUE_SEPARATOR = " ";
+	private static final String VALUE_SEPARATOR = " ";
 	// 配列の値を出力する際の括弧開きを設定する
-	public static final String FIRST_OUTPUT = "{";
+	private static final String FIRST_OUTPUT = "{";
 	// 配列の値を出力する際の閉じ括弧を設定する
-	public static final String LAST_OUTPUT = "}";
+	private static final String LAST_OUTPUT = "}";
 	// 乱数の範囲を設定する
-	public static final int RANGE_RANDOM = 10;
+	private static final int RANGE_RANDOM = 10;
 	// 配列の末尾のインデックスを取得するための値を設定する
-	public static final int SHIFT_LAST_INDEX = -1;
+	private static final int SHIFT_LAST_INDEX = -1;
 	
 	
 	/**********************************************************/
@@ -42,7 +40,7 @@ public class E6_7_GetLastArrayValue {
 		int arrayLength = INITIALIZE_NUMERIC;
 
 		// 要素数の入力を扱うオブジェクトを作成する
-		Scanner lengthInput = new Scanner(System.in);
+		Scanner ScannerArraylength = new Scanner(System.in);
 		// 配列に設定する乱数を生成するオブジェクトを作成する
 		Random arrayValueGenerator = new Random();
 		
@@ -52,7 +50,7 @@ public class E6_7_GetLastArrayValue {
 		// 要素数の入力を求めるメッセージを出力する
 		System.out.println(REQUEST_ARRAY_LENGTH);
 		// 入力された要素数を保持する
-		arrayLength = lengthInput.nextInt();
+		arrayLength = ScannerArraylength.nextInt();
 		// 保持した要素数から配列を作成する
 		int[] randomArray = new int[arrayLength];
 		// 配列に値を設定していく
@@ -60,6 +58,9 @@ public class E6_7_GetLastArrayValue {
 			// 設定した範囲の乱数を設定する
 			randomArray[count] = arrayValueGenerator.nextInt(RANGE_RANDOM);
 		}
+		
+		// 出力内容を表示する
+		System.out.println(MESSAGE_OUTPUT_ALL);
 		
 		// 配列の値の出力を囲む括弧を出力する
 		System.out.print(FIRST_OUTPUT);
@@ -81,6 +82,6 @@ public class E6_7_GetLastArrayValue {
 		/***************************************************/
 
 		// 要素数の入力ストリームを閉じる
-		lengthInput.close();
+		ScannerArraylength.close();
 	}
 }
