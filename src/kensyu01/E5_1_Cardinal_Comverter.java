be2d7a35590a0da05f9d873050e70c54428ccc8f
ddt-nishiwaki <ddt.jissyusei2@gmail.com>
2017-05-12 19:27:09 +0900

    演習5-1 修正100% : 定数をprivateにする, スキャナーの名前を具体的にする

--- src/kensyu01/E5_1_Cardinal_Comverter.java
+++ src/kensyu01/E5_1_Cardinal_Comverter.java
@@ -9,15 +9,15 @@ import java.util.Scanner;
 public class E5_1_Cardinal_Comverter {
 	
 	// int型変数を初期化するための値を設定する
-	public static final int INITIALIZE_INTEGER = 0;
+	private static final int INITIALIZE_INTEGER = 0;
 	// 処理内容を示すメッセージを設定する
-	public static final String MESSAGE_PROCESSING = "入力した値を8進数、16進数で表示します。";
+	private static final String MESSAGE_PROCESSING = "入力した値を8進数、16進数で表示します。";
 	// 整数値の入力を求めるメッセージを設定する
-	public static final String MESSAGE_REQEST_INPUT = "整数値を入力してください";
+	private static final String MESSAGE_REQEST_INPUT = "整数値を入力してください";
 	// 入力された値を8進数で表示することをメッセージで出力する
-	public static final String MESSAGE_OUT_OCTAL = "8進数 => : ";
+	private static final String MESSAGE_OUT_OCTAL = "8進数 => : ";
 	// 入力された値を16進数で表示することをメッセージで出力する
-	public static final String MESSAGE_OUT_HEXADECIMAL = "16進数 => : ";
+	private static final String MESSAGE_OUT_HEXADECIMAL = "16進数 => : ";
 	
 	
 	/********************************************************/
@@ -28,7 +28,7 @@ public class E5_1_Cardinal_Comverter {
 		// 入力された値を保持する変数を初期化する
 		int integerValue = INITIALIZE_INTEGER;
 		// 整数値の入力を扱うオブジェクトを作成する
-		Scanner integerInput = new Scanner(System.in);
+		Scanner scannerConvertOperand = new Scanner(System.in);
 		
 		/********************************************************/
 
@@ -37,7 +37,7 @@ public class E5_1_Cardinal_Comverter {
 		// 整数値の入力を求めるメッセージを出力する
 		System.out.println(MESSAGE_REQEST_INPUT);
 		// 入力された値を保持する
-		integerValue = integerInput.nextInt();
+		integerValue = scannerConvertOperand.nextInt();
 		// 入力された値を8進数で表示する
 		System.out.printf(MESSAGE_OUT_OCTAL+"%o\n", integerValue);
 		// 入力された値16を進数で表示する
@@ -47,6 +47,6 @@ public class E5_1_Cardinal_Comverter {
 		/********************************************************/
 
 		// 整数入力のストリームを閉じる
-		integerInput.close();
+		scannerConvertOperand.close();
 	}
 }
