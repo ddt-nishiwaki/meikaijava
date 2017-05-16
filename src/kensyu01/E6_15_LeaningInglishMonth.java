40142b9ba7edf9f14c6d7fe4cd6d077495a08874
ddt-nishiwaki <ddt.jissyusei2@gmail.com>
2017-05-17 07:22:22 +0900

    演習6-14 修正100% : 変数名の修正を行う

--- src/kensyu01/E6_15_LeaningInglishMonth.java
+++ src/kensyu01/E6_15_LeaningInglishMonth.java
@@ -32,25 +32,27 @@ public class E6_15_LeaningInglishMonth {
 	// 文字列型変数を初期化する値を設定する
 	private static final String INITIALIZE_STRING = "";
 	// 出題の内容の内容を設定する
-	private static final String MESSAGE_REQEST_ANSWER = "英語の月名を描いてください";
+	private static final String MESSAGE_REQUEST_ANSWER = "英語の月名を描いてください";
 	// 回答のルールを設定する
 	private static final String MESSAGE_RULE_INPUT = "なお、先頭は大文字で、2文字目以降は小文字とします";
 	// 出題形式のフォーマットを設定する
 	private static final String FORMAT_OUTPUT_QUESTION = "%d月を英語で書いてください : ";
 	// 正解時のメッセージを設定する
-	private static final String MESSAGE_COMPLETE = "正解です";
+	private static final String MESSAGE_CORRECT_ANSWER = "正解です";
 	// 不正解時のメッセージを設定する
-	private static final String MESSAGE_FAILD = "不正解です";
+	private static final String MESSAGE_INCORRECT_ANSWER = "不正解です";
 	// 続けるかどうかのメッセージのフォーマットを設定する
-	private static final String FORMAT_REQEST_REPLAY = "もう一度やりますか？ yes ... %d , no ... %d\n";
+	private static final String FORMAT_REQUEST_REPLAY = "もう一度やりますか？ yes ... %d , no ... %d\n";
 	// 学習を終了するメッセージを出力する
-	private static final String MESSAGE_END_LEANING = "学習を終了します";
+	private static final String MESSAGE_END_LEARNING = "学習を終了します";
 	// 処理を繰り返すための値を設定する
 	private static final int VALUE_REPLAY = 1;
 	// 処理を終了するための値を設定する
 	private static final int VALUE_END_PLAY = 0;
 	// 月を設定する配列の長さを設定する
 	private static final int VALUE_RANGE_ARRAY_MONTH = 12;
+	// 生成された乱数を実際の月になおす値を設定する
+	private static final int VALUE_FORMAT_RAMDOM_RANGE = 1;
 
 
 	/*** メソッド ************************/
@@ -63,69 +65,74 @@ public class E6_15_LeaningInglishMonth {
 		// 正解を設定する変数を初期化する
 		String wordCorrect = INITIALIZE_STRING;
 		// 回答された答えを保持する変数を初期化する
-		String wordAnswred = INITIALIZE_STRING;
+		String wordAnswered = INITIALIZE_STRING;
 		// 繰り返すかどうかのコマンドを保持する変数を初期化する
 		int valueUserCommand = INITIALIZE_NUMBER;
 		// 問題になる月を設定する値を保持する変数を初期化する
-		int valueQuestionMonth = INITIALIZE_NUMBER;
-
-
+		int indexQuestionMonth = INITIALIZE_NUMBER;
+		// 実際の月の値を保持する変数を初期化する
+		int numberQuestionMonth = INITIALIZE_NUMBER;
 		/*** オブジェクトの作成 ************************/
 		// ランダムに月を設定するオブジェクトを作成する
 		Random generatorRandomMonth = new Random();
 		// 回答入力を扱うオブジェクトを作成する
-		Scanner scannerAnser = new Scanner(System.in);
+		Scanner scannerAnswer = new Scanner(System.in);
 		// 繰り返すかどうかのコマンド入力を扱うオブジェクトを作成する
 		Scanner scannerCommandReplay = new Scanner(System.in);
 
 		/*** 処理 ************************/
 		// 英語の入力を求めるメッセージを出力する
-		System.out.println(MESSAGE_REQEST_ANSWER);
+		System.out.println(MESSAGE_REQUEST_ANSWER);
 		// 入力に関するルールを出力する
 		System.out.println(MESSAGE_RULE_INPUT);
 
 		// 問題を出題する
 		do {
 			// 1月から12月までのうちのいずれかを問題に設定する
-			valueQuestionMonth = generatorRandomMonth.nextInt(VALUE_RANGE_ARRAY_MONTH);
+			indexQuestionMonth = generatorRandomMonth.nextInt(VALUE_RANGE_ARRAY_MONTH);
+			// 設定した問題のインデックスを実際の月に変換する
+			numberQuestionMonth = indexQuestionMonth + VALUE_FORMAT_RAMDOM_RANGE;
 			// 設定した課題が前回と同じ場合は設定を繰り返す
-			while ( valueQuestionMonth == valuePreviousQuestion ) {
+			while ( indexQuestionMonth == valuePreviousQuestion ) {
 				// 問題を再設定する
-				valueQuestionMonth = generatorRandomMonth.nextInt(VALUE_RANGE_ARRAY_MONTH);
+				indexQuestionMonth = generatorRandomMonth.nextInt(VALUE_RANGE_ARRAY_MONTH);
+				// 設定した問題のインデックスを実際の月に変換する
+				numberQuestionMonth = indexQuestionMonth + VALUE_FORMAT_RAMDOM_RANGE;
 			}
 			// 出題内容を次回まで保持する
-			valuePreviousQuestion = valueQuestionMonth;
+			valuePreviousQuestion = indexQuestionMonth;
 
 			// 正解を設定する
-			wordCorrect = LIST_WORD_MONTH[valueQuestionMonth];
+			wordCorrect = LIST_WORD_MONTH[indexQuestionMonth];
 			
-			// 問題を出力する
-			System.out.printf(FORMAT_OUTPUT_QUESTION, ++valueQuestionMonth);
-			// 回答を保持する
-			wordAnswred = scannerAnser.next();
-			
-			// 正解した場合の処理を行う
-			if ( wordAnswred.equals(wordCorrect) ){
-				// 正解のメッセージを出力する
-				System.out.println(MESSAGE_COMPLETE);
-			// 不正解の場合の処理を行う
-			} else {
-				// 不正解のメッセージを出力する
-				System.out.println(MESSAGE_FAILD);
-			}
+			// 正解するまで出題を繰り返す
+			do {
+				// 月の名前を英語で答えさせる問題を出力する
+				System.out.printf(FORMAT_OUTPUT_QUESTION, numberQuestionMonth);
+				// 回答を保持する
+				wordAnswered = scannerAnswer.next();
+				
+				// 不正解の場合の処理を行う
+				if ( !(wordAnswered.equals(wordCorrect)) ){
+					// 不正解のメッセージを出力する
+					System.out.println(MESSAGE_INCORRECT_ANSWER);
+				}
+			} while (!(wordAnswered.equals(wordCorrect)));
+			// 正解のメッセージを出力する
+			System.out.println(MESSAGE_CORRECT_ANSWER);
 			// もう一度行うかどうかを確認するメッセージを出力する
-			System.out.printf(FORMAT_REQEST_REPLAY,VALUE_REPLAY,VALUE_END_PLAY);
+			System.out.printf(FORMAT_REQUEST_REPLAY,VALUE_REPLAY,VALUE_END_PLAY);
 			// コマンドを保持する
 			valueUserCommand = scannerCommandReplay.nextInt();
 			// もう一度行うコマンドが入力された場合は繰り返す
 		} while ( valueUserCommand == VALUE_REPLAY);
 		
 		// 学習を終了することをメッセージで出力する
-		System.out.println(MESSAGE_END_LEANING);
+		System.out.println(MESSAGE_END_LEARNING);
 		
 		/*** 終了処理 ************************/
 		// 回答入力用ストリームを閉じる
-		scannerAnser.close();
+		scannerAnswer.close();
 		// コマンド入力用ストリームを閉じる
 		scannerCommandReplay.close();
 		
