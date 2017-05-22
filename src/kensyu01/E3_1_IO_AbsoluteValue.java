3a1bab757eafffed16ff18784d36a299e51f632e
ddt-nishiwaki <ddt.jissyusei2@gmail.com>
2017-05-23 08:32:31 +0900

    演習3-1 修正 100% : コメント抜け訂正

--- src/kensyu01/E3_1_IO_AbsoluteValue.java
+++ src/kensyu01/E3_1_IO_AbsoluteValue.java
@@ -6,13 +6,18 @@ import java.util.Scanner;
 // 入力した値の絶対値を出力するクラス
 public class E3_1_IO_AbsoluteValue {
 
-
+	// 数値型変数を初期化する値を設定する
 	private static final int INITIALIZE_NUMBER = 0;
+	// 処理内容を示すメッセージを設定する
 	private static final String MESSAGE_OUTPUT_PROCESS_DETAIL = "入力された値の絶対値を調べます";
+	// 整数値の入力を求めるメッセージを設定する
 	private static final String MESSAGE_REQUEST_INPUT_INTEGER = "整数値を入力 : ";
+	// 入力された値の絶対値を示すメッセージのフォーマットを設定する
 	private static final String FORMAT_OUTPUT_ABSOLUTE_NUMBER = "その絶対値は%dです\n";
+	// 入力が不正な場合のメッセージを設定する
 	private static final String MESSAGE_CAUTION_INPUT_INVAILD = "入力が不正です";
 	
+
 	// 整数値の入力を確実に行うメソッド
 	public static int scanInteger ( Scanner scannerInteger ) {
 		// 入力を保持する変数を初期化する
@@ -25,6 +30,7 @@ public class E3_1_IO_AbsoluteValue {
 				inputValue = scannerInteger.nextInt();
 				// ループ処理を終了する
 				break;
+			// 入力された値が整数値じゃ勝った場合の処理を行う
 			} else {
 				// 入力が間違えていることを出力する
 				System.out.println(MESSAGE_CAUTION_INPUT_INVAILD);
@@ -33,15 +39,18 @@ public class E3_1_IO_AbsoluteValue {
 				// 入力ストリームのトークンを次に移動する
 				scannerInteger.next();
 			}
+		// 整数値が設定されるまで繰り返す
 		} while ( inputValue == INITIALIZE_NUMBER);
 		
+		// 設定された整数値を返す
 		return inputValue;
 	}
 	
 	
 	// 入力した値を絶対値で出力する為の処理を行う
 	public static void main (String[] arg){
-		int inputValue = 0;
+		// 読み込んだ整数値を保持する変数を初期化する
+		int inputValue = INITIALIZE_NUMBER;
 		// 入力した値を読み込む為のオブジェクトを作成する
 		Scanner scannerInteger = new Scanner(System.in);
 		
@@ -49,13 +58,10 @@ public class E3_1_IO_AbsoluteValue {
 		System.out.println(MESSAGE_OUTPUT_PROCESS_DETAIL);
 		// 整数値の入力を促すメッセージを出力する為の処理する
 		System.out.println(MESSAGE_REQUEST_INPUT_INTEGER);
-		// 入力された値を保持する変数を初期化する	
 		
 		// 整数値を確実に読み込む
 		inputValue = scanInteger(scannerInteger);
 		
-		// 入力用ストリームを閉じる
-		scannerInteger.close();
 		
 		// 入力された値が0未満の場合 -1 を乗算して絶対値に変換する為の処理を行う
 		if( inputValue < 0 ) {
@@ -67,6 +73,8 @@ public class E3_1_IO_AbsoluteValue {
 		// 変数inputValueを出力する為の処理を行う
 		System.out.printf(FORMAT_OUTPUT_ABSOLUTE_NUMBER, inputValue);
 		
+		// 入力用ストリームを閉じる
+		scannerInteger.close();
 	}
 		
 }
