0b12570a274859536430fab526c9e41f61979258
ddt-nishiwaki <ddt.jissyusei2@gmail.com>
2017-05-23 23:07:41 +0900

    演習7-1 修正 100% : コメント修正、定数名修正、変数宣言を変数初期化に変更

--- src/kensyu01/E7_1_SignOfMethod.java
+++ src/kensyu01/E7_1_SignOfMethod.java
@@ -8,21 +8,21 @@ import java.util.Scanner;
 // 整数が負の値か正の値か判定するメソッドを持つクラス
 public class E7_1_SignOfMethod {
 
-	/*** 定数設定 *******************************************/
+	/*** 数値型定数設定 *******************************************/
 	// 数値型変数を初期化する値を設定する
-	private static final int INITIALIZE_NUMBER = 0;
+	private static final int INITIALIZE_ZERO = 0;
 	// 判定結果が正であることを示す値を設定する
 	private static final int VALUE_JUDGE_POSITIVE_SIGN = 1;
 	// 判定結果が負であることを示す値を設定する
 	private static final int VALUE_JUDGE_NEGATIVE_SIGN = -1;
 	
+	/*** 文字列型定数設定 *******************************************/
 	// 処理内容を出力するためのフォーマットを設定する
 	private static final String FORMAT_OUTPUT_PROCESS_DETAIL = "入力さた整数が%sの値か%sの値か判定します\n";
 	// 正負を判定する整数値の入力を求めるメッセージを出力する
 	private static final String MESSAGE_REQUEST_INTEGER_NUMBER = "整数を入力してください : ";
 	// 入力された数値の正負判定の結果を出力するフォーマットを設定する
 	private static final String FORMAT_OUTPUT_RESULT_SIGN = "入力された値は%sです\n";
-	
 	// テキスト「正」を設定する
 	private static final String WORD_POSITIVE_SIGN = "正";
 	// テキスト「負」を設定する
@@ -31,11 +31,13 @@ public class E7_1_SignOfMethod {
 	private static final String WORD_ZERO = "0";
 	
 	
-	/*** 正負判定メソッド *******************************************/
-	// 受け取ったint型引数が 正の値か負の値か 判定する
-	public static int signOf ( int inputNumber ) {
+	/*** メソッド *******************************************/
+	// 受け取ったint型引数が 正の値か負の値か 判定するメソッドを設定する
+	private static int signOf ( int inputNumber ) {
+
 		/*** 変数初期化 *******************************************/
-		int resultInputSign;		// 正負の判定結果を保持する変数を初期化する
+		// 正負の判定結果を保持する変数を初期化する
+		int resultInputSign = INITIALIZE_ZERO;		
 
 		/*** 処理 *******************************************/
 		// 受け取った引数の正負を 0 で判定し、正だった場合の処理を行う
@@ -61,26 +63,30 @@ public class E7_1_SignOfMethod {
 	/*** メインメソッド *******************************************/
 	// 整数が負の値か正の値か判定する処理を行う
 	public static void main (String[] arg){
+
 		/*** 変数初期化 *******************************************/
 		// 正負を判定するために入力された値を保持する変数を初期化する
-		int targetNumberJadgeSign = INITIALIZE_NUMBER;
+		int targetNumberJadgeSign = INITIALIZE_ZERO;
 		// 正負の判定結果を保持する変数を初期化する
-		int resultJadgeSign = INITIALIZE_NUMBER;
+		int resultJadgeSign = INITIALIZE_ZERO;
 
 		/*** オブジェクト作成 *******************************************/
 		// 正負を判定する整数値の入力を扱うオブジェクトを作成する
 		Scanner scannerNumberJadgeSign = new Scanner(System.in);
 	
-		/*** 処理 *******************************************/
+		/*** 入力処理 *******************************************/
 		// 処理内容を出力する
 		System.out.printf(FORMAT_OUTPUT_PROCESS_DETAIL, WORD_POSITIVE_SIGN, WORD_NEGATIVE_SIGN);
 		// 整数値の入力を求める
 		System.out.println(MESSAGE_REQUEST_INTEGER_NUMBER);
 		// 入力された値を保持する
 		targetNumberJadgeSign = scannerNumberJadgeSign.nextInt();
+
+		/*** 正負判定処理 *******************************************/
 		// 入力された値の正負を判定する
 		resultJadgeSign = signOf(targetNumberJadgeSign);
 		
+		/*** 出力処理 *******************************************/
 		// 入力された値が正だった場合の処理を行う
 		if ( resultJadgeSign == VALUE_JUDGE_POSITIVE_SIGN ) {
 			// 正の値であることを表示する
@@ -98,6 +104,5 @@ public class E7_1_SignOfMethod {
 		/*** 終了処理 *******************************************/
 		// 正負を判定する整数値の入力用ストリームを閉じる
 		scannerNumberJadgeSign.close();
-
 	}
 }
