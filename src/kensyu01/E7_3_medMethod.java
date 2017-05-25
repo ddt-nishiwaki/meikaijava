cc218b80fe4daaa29a4c75f24fd8d625350b6e57
ddt-nishiwaki <ddt.jissyusei2@gmail.com>
2017-05-25 19:04:44 +0900

    演習7-3 修正 100% : 定数名変更、medメソッドのロジック変更、不要な定数を削除、コメントの修正

--- src/kensyu01/E7_3_medMethod.java
+++ src/kensyu01/E7_3_medMethod.java
@@ -8,15 +8,12 @@ import java.util.Scanner;
 /*** クラス ***********************************/
 // 入力された3つの値のうち中央値を出力するメソッドを持つクラス
 public class E7_3_medMethod {
-	
-	/*** 定数設定 ***********************************/
-	// 数値型変数を初期化する値を設定する
-	private static final int INITIALIZE_NUMBER = 0;
-	// 入力する値の数を設定する
-	private static final int LENGTH_INPUT_ARGUMENTS = 3;
-	// 現在のインデックスを次のインデックスにずらす値を設定する
-	private static final int VALUE_SHIFT_NEXT = 1;
 
+	/*** 数値型定数設定 ***********************************/
+	// 数値型変数を初期化する値を設定する
+
+	private static final int INITIALIZE_ZERO = 0;
+	/*** 文字列型定数設定 ***********************************/
 	// 処理内容を示すメッセージを設定する
 	private static final String MESSAGE_OUTPUT_PROCESS_DETAIL = "入力した数値のうち、中央値を判定します";
 	// 値の入力を求めるメッセージを設定する
@@ -31,37 +28,19 @@ public class E7_3_medMethod {
 	private static final String FORMAT_OUTPUT_MIN_NUMBER = "中央の値は%dです\n";
 	
 
-	/*** 中央値を出力するメソッド ***********************************/
+	/*** メソッド ***********************************/
 	// 入力された3つの値のうち中央値を出力するメソッドを設定する
-	public static int med ( int firstValue, int secondValue, int thirdValue ) {
+	private static int med ( int firstValue, int secondValue, int thirdValue ) {
+
 		/*** 変数の初期化 ***********************************/
-		// 引数を保持する配列を初期化する
-		int[] listMedArguments = {
-				firstValue,
-				secondValue,
-				thirdValue
-		};
 		// 現在の値を保持する変数を初期化する
-		int medianValue = INITIALIZE_NUMBER;
+		int medianValue = INITIALIZE_ZERO;
 		
-		/*** 処理 ***********************************/
-		// 配列を昇順にソートする
-		for ( int indexCount = INITIALIZE_NUMBER; indexCount < LENGTH_INPUT_ARGUMENTS; indexCount++ ) {
-			// 現在の値を一時的に変数に保持する
-			int temp = listMedArguments[indexCount];
-			// 現在の値と他の値を比べる
-			for ( int nextIndexCount = indexCount + VALUE_SHIFT_NEXT; nextIndexCount < LENGTH_INPUT_ARGUMENTS; nextIndexCount++ ){	
-				// 現在の値が他の値より大きい場合の処理を行う
-				if ( listMedArguments[indexCount] > listMedArguments[nextIndexCount] ) {
-					// 現在の値を他の値に置き換える
-					listMedArguments[indexCount] = listMedArguments[nextIndexCount];
-					// 他の値に一時的に保持しておいた現在の値を設定する
-					listMedArguments[nextIndexCount] = temp;
-				}
-			}
-		}
-		// 配列のインデックスの中央の値を取得する
-		medianValue = listMedArguments[LENGTH_INPUT_ARGUMENTS/2];
+		/*** 演算処理 ***********************************/
+		// 一つ目の値と二つ目の値のうち、小さくない方の値を保持する
+		medianValue = ( firstValue >= secondValue ) ? firstValue : secondValue;
+		// 保持した値と三つ目の値のを比べて、大きくない方の値を保持する
+		medianValue = ( medianValue >= thirdValue ) ? thirdValue : medianValue;
 		
 		/*** 終了処理 ***********************************/
 		// 中央値を返す
@@ -72,19 +51,22 @@ public class E7_3_medMethod {
 	/*** メインメソッド ***********************************/
 	// 入力された3つの値のうち中央値を出力する処理を行う
 	public static void main (String[] arg ) {
-		// 一つ目の入力を保持する変数を初期化する
-		int firstValue = INITIALIZE_NUMBER;
-		// 二つ目の入力を保持する変数を初期化する
-		int secondValue = INITIALIZE_NUMBER;
-		// 三つ目の入力を保持する変数を初期化する
-		int thirdValue = INITIALIZE_NUMBER;
-		// 中央値を保持する変数を初期化する
-		int resultMedValue = INITIALIZE_NUMBER;
+
+		/*** 変数初期化 ***********************************/
+		// 一つ目の入力を保持する変数を 0　で初期化する
+		int firstValue = INITIALIZE_ZERO;
+		// 二つ目の入力を保持する変数を 0　で初期化する
+		int secondValue = INITIALIZE_ZERO;
+		// 三つ目の入力を保持する変数を 0　で初期化する
+		int thirdValue = INITIALIZE_ZERO;
+		// 中央値を保持する変数を 0　で初期化する
+		int resultMedValue = INITIALIZE_ZERO;
+
 		/*** オブジェクトの作成 ***********************************/
 		// 引数設定の入力を扱うオブジェクトを作成する
 		Scanner scannerArguments = new Scanner(System.in);
 		
-		/*** 処理 ***********************************/
+		/*** 入力処理 ***********************************/
 		// 処理内容を出力する
 		System.out.println(MESSAGE_OUTPUT_PROCESS_DETAIL);
 		// 3つの値の入力を求めるメッセージを出力する
@@ -102,6 +84,7 @@ public class E7_3_medMethod {
 		// 三つ目の入力を保持する
 		thirdValue = scannerArguments.nextInt();
 		
+		/*** 出力処理 ***********************************/
 		// 入力された値のうちの中央値を求める
 		resultMedValue = med(firstValue, secondValue, thirdValue);
 		// 求めた中央値を出力する
