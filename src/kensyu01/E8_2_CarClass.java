8300125f2789fef2fd0622fa6f939d072d0025ae
ddt-nishiwaki <ddt.jissyusei2@gmail.com>
2017-06-09 06:40:14 +0900

    演習8-2 100%

--- src/kensyu01/E8_2_CarClass.java
+++ src/kensyu01/E8_2_CarClass.java
@@ -26,27 +26,54 @@ class Car {
 	private final int NUMBER_STATUS_DRIVE = 1;
 	// 駐車することを示す値を設定する
 	private final int NUMBER_STATUS_PARK = 0;
-	
 	// アクセルと踏むコマンドを設定する
 	private final int NUMBER_STEP_ON_COMMAND = 1;
 	// 給油するコマンドを設定する
 	private final int NUMBER_REFUEL_COMMAND = 2;
 	// ガソリン残量を出力するコマンドを設定する
-	private final int NUMBER_OUTPUT_MILAGE_COMMAND = 3;
+	private final int NUMBER_OUTPUT_MILEAGE_COMMAND = 3;
 	// 駐車する(終了する)コマンドを設定する
 	private final int NUMBER_PARK_COMMAND = 4;
-	
+	// 走行距離を決定する範囲を設定する
+	private final int NUMBER_MILEAGE_RANGE = 100;
+	// 乱数の範囲を出力用の表示に変換する値を設定する
+	private final int NUMBER_FORMATING_OUTPUT_RANDOM = 1;
+
 	/*** 文字列型定数設定 ***********************/
 	// 運転を開始することを示すメッセージを設定する
 	private static String MESSAGE_START_DRIVE = "車の運転を開始します";
 	// 給油することを促すメッセージを表示する
 	private static String MESSAGE_REQUEST_REFUEL = "どのくらい給油しますか？";
-			
+	// 走行距離を出力するためのフォーマットを設定する
+	private static String FORMAT_OUTPUT_MILEAGE = "%d Km\n";
+	// 給油を促すメッセージを設定する
+	private static String CAUTION_REQUEST_REFUEL = "!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
+												 + "燃料が足りません 給油してください\n"
+												 + "!!!!!!!!!!!!!!!!!!!!!!!!!!!"; 
+	// ガソリンの残量を表示するためのフォーマットを設定する
+	private static String FORMAT_OUTPUT_FUEL = "ガソリンの残量 : %5.2f L\n";
+	// 操作の選択を求めるメッセージを選択する
+	private static String MESSAGE_REQUEST_COMMAND = "操作を選択してください";
+	// 実行可能なコマンドを表示するためのフォーマットを設定する
+	private static String FORMAT_OUTPUT_COMMANDS = "1:%s  2:%s  3:%s  4:%s";
+	// テキスト「走行する」を設定する
+	private static String TEXT_STEP_ON = "走行する";
+	// テキスト「給油する」を設定する
+	private static String TEXT_REFUEL = "給油する";
+	// テキスト「走行距離を見る」を設定する
+	private static String TEXT_OUTPUT_MILEAGE = "走行距離を確認する";
+	// テキスト「駐車（終了）する」を設定する
+	private static String TEXT_PARK = "駐車（終了)する";
+	//　読み取れないコマンドが入力された場合のメッセージを設定する
+	private static String CAUTION_NOT_COMMAND = "コマンドが読み取れません。もう一度入力してください";
+	// 運転を終了するメッセージを設定する
+	private static String MESSAGE_END_DRIVE = "運転を終了します";
+
 	/*** スキャナーを設定する ***********************/
 	// 入力を受け取るためのスキャナーを設定する
 	private Scanner scannerInput = new Scanner(System.in);
 	// 走行距離を決定するオブジェクトを作成する
-	private Random generateMilage = new Random();
+	private Random generateMileage = new Random();
 	
 	/*** コンストラクタ ***********************/
 	// 燃料をセットして車オブジェクトを作成する
@@ -56,33 +83,22 @@ class Car {
 		this.fuel = (float)fuel;			
 	}
 	
+	/*** セッター ***********************/
+	// 燃料を追加するメソッドを設定する
+	private void setFuel (float setFuel){
+		// 設定された燃料を加算する
+		this.fuel += setFuel;
+	}
+	// 駐車する(終了)メソッドを設定する
+	private void setParkStatus(){
+		// 車の状態を「駐車」に変更する
+		this.status = NUMBER_STATUS_PARK;
+	}
 	/*** メソッド ***********************/
-	
-	/*
-	 *********************************************
-	 * 現在以下のようなメソッドを設定しています
-	 * 機能を持ったメソッドと、それを実現するためのメソッドは
-	 * 次のような振り分けで認識あっていますでしょうか？
-	 * ご確認宜しくお願い致します。
-	 *********************************************
-	 * （機能を持ったメソッド）
-	 * 運転を開始するメソッド
-	 * 運転を終了（停車）するメソッド
-	 * 走行するメソッド
-	 * 給油するメソッド
-	 * 走行距離を見るメソッド
-	 * コマンド一覧を表示するメソッド
-	 * コマンド入力を振り分けるメソッド
-	 * 
-	 * （機能実現のためのメソッド）
-	 * 燃料を消費するメソッド
-	 * 残燃料を表示するメソッド
-	 * 
-	 */
 	// 総走行距離を出力するメソッドを設定する
-	private void outputMilage(){
+	private void outputMileage(){
 		// 総走行距離を出力する
-		System.out.printf("%d Km\n",this.mileage);
+		System.out.printf(FORMAT_OUTPUT_MILEAGE,this.mileage);
 	}
 	// 燃料を消費するメソッドを設定する
 	private void reduceFuel(float usedFuel){
@@ -93,12 +109,12 @@ class Car {
 		// 燃料が使用量に満たない場合の処理を行う
 		} else {
 			// 給油することを促すメッセージを表示する
-			System.out.println("燃料が足りません 給油してください");
+			System.out.println(CAUTION_REQUEST_REFUEL);
 		}
 	}
 	// 残燃料を表示するメソッドを設定する
 	private void outputFuelIndicator() {
-		System.out.printf("ガソリンの残量 : %5.2f L\n", this.fuel);
+		System.out.printf(FORMAT_OUTPUT_FUEL, this.fuel);
 	}
 	// 走行するメソッドを設定する
 	private void stepOn(){
@@ -106,12 +122,12 @@ class Car {
 		// 使用する燃料を設定する変数を宣言する
 		float usedFuel;
 		// 1km ~ 10km までのランダムな走行距離を設定する
-		int milageValue = generateMilage.nextInt(100) -1;
+		int mileageValue = generateMileage.nextInt(NUMBER_MILEAGE_RANGE) + NUMBER_FORMATING_OUTPUT_RANDOM;
 		/*** 処理 *********************************/
 		// 距離を走行距離に設定する 
-		this.mileage += milageValue;
+		this.mileage += mileageValue;
 		// 距離に応じた量の燃料を設定する
-		usedFuel = (float)milageValue/this.fuelEfficiency;
+		usedFuel = (float)mileageValue/this.fuelEfficiency;
 		// 設定した燃料を減らす
 		this.reduceFuel(usedFuel);
 	}
@@ -138,23 +154,26 @@ class Car {
 			// 満タンにすることをメッセージで出力する
 			System.out.println("満タンにします");
 			// 燃料を満タンにする
-			this.fuel = this.fuelCapacity;
+			this.setFuel(this.fuelCapacity);
 		}
 	}
-	// 駐車する(終了)メソッドを設定するメソッド
-	private void park(){
-		// 車の状態を「駐車」に変更する
-		this.status = NUMBER_STATUS_PARK;
+	// 駐車するメソッドを設定する
+	private void park() {
+		// 運転を終了するメッセージを出力する
+		System.out.println(MESSAGE_END_DRIVE);
+		this.setParkStatus();
 	}
-	// 実行可能なコマンドを出力するメソッド
+	// 実行可能なコマンドを出力する
 	private void outputCommands(){
 		// 操作の入力を求めるメッセージを出力する
-		System.out.println("操作を選択してください");
+		System.out.println(MESSAGE_REQUEST_COMMAND);
 		// 実行できるコマンドを表示する
-		System.out.println("1:走行する  2:給油する  3:走行距離を見る  4:停車する");
+		System.out.printf(FORMAT_OUTPUT_COMMANDS,
+				TEXT_STEP_ON, TEXT_REFUEL, TEXT_OUTPUT_MILEAGE, TEXT_PARK);
 	}
-	// 入力されたコマンドから処理を振り分けるメソッドを設定するメソッド
+	// 車の操作を行うメソッドを設定する
 	private void controlCar(int command){
+		// コマンド毎に処理を振り分ける
 		switch (command) {
 			// 走行する　コマンドが入力された場合の処理を行う
 			case NUMBER_STEP_ON_COMMAND :
@@ -169,9 +188,9 @@ class Car {
 				// スイッチ文を終了する
 				break;
 			// 走行距離をみる　コマンドが入力された場合の処理を行う
-			case NUMBER_OUTPUT_MILAGE_COMMAND :
+			case NUMBER_OUTPUT_MILEAGE_COMMAND :
 				// 現在の総走行距離を出力する
-				this.outputMilage();
+				this.outputMileage();
 				// スイッチ文を終了する
 				break;
 			// 停車する　コマンドが入力された場合の処理を行う
@@ -183,7 +202,7 @@ class Car {
 			// 実行可能なコマンド以外が入力された場合の処理を行う
 			default :
 				// コマンドが読み取れないことを表示する
-				System.out.println("コマンドが読み取れません。もう一度入力してください");
+				System.out.println(CAUTION_NOT_COMMAND);
 		}
 	}
 	// 運転するメソッドを設定する
@@ -202,19 +221,24 @@ class Car {
 			this.controlCar( scannerInput.nextInt() );
 		// 駐車するまで運転を続ける
 		} while ( this.status != NUMBER_STATUS_PARK);
-		// 運転を終了するメッセージを出力する
-		System.out.println("運転を終了します");
+		// 入力用ストリームを閉じる
+		this.scannerInput.close();
 	}
 }
 
+/*** クラス ******************************/
 // Carクラスからインスタンスを作成してメソッドを実行するクラス
 public class E8_2_CarClass {
 
+	/*** メインメソッド ******************************/
 	// Carクラスからインスタンスを作成してメソッドを実行する処理を行う
 	public static void main(String[] arg){
 
+		/*** オブジェクト作成 ******************************/
 		// Carオブジェクト を作成する
 		Car myCar = new Car(200);
+		
+		/*** 処理 ******************************/
 		// 車を運転する
 		myCar.drive();
 	}
