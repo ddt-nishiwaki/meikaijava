d9c8d148685f41df417dbffa3e3d5896d22e4ae1
ddt-nishiwaki <ddt.jissyusei2@gmail.com>
2017-07-07 20:27:37 +0900

    DVDプレーヤークラスを作成

---
+++ src/player/DVDPlayer.java
@@ -0,0 +1,38 @@
+package player;
+
+
+/**
+ * DVDプレーヤを作成するクラス
+ * 再生,スロー再生,停止メソッドを持ちます
+ * @author wakkky05
+ * @see ExPlayer
+ * @see Player
+ */
+public class DVDPlayer implements ExPlayer{
+
+	/****************************************
+		定数(String)
+	****************************************/
+	private static final String MEDIA_NAME = "DVD";
+	
+	/**
+	 * 再生を行うことをメッセージ出力します
+	 */
+	public void mPlay(){
+		System.out.printf(FORMAT_PLAY,MEDIA_NAME);
+	}
+
+	/**
+	 * 停止を行うことをメッセージ出力します
+	 */
+	public void mStop(){
+		System.out.printf(FORMAT_STOP,MEDIA_NAME);
+	}
+
+	/**
+	 * スロウ再生を行うことをメッセージ出力します
+	 */
+	public void mSlow(){
+		System.out.printf(FORMAT_SLOW_PLAY,MEDIA_NAME);
+	}
+}
